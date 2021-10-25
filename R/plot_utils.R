c1571fe7e0f2b2b31179855acff22b6027edd094
Massimo <mcavallaro@users.noreply.github.com>
2021-10-25 15:20:51 +0100

    update plot_utils.R

--- R/plot_utils.R
+++ R/plot_utils.R
@@ -1,6 +1,13 @@
-library(sf)
-library(spatstat)
-
+tab.blue = "#1f77b4"
+tab.orange = "#ff7f0e"
+tab.green = "#2ca02c"
+tab.red = "#d62728"
+tab.purple = "#9467bd"
+tab.brown = "#8c564b"
+tab.pink = "#e377c2"
+tab.gray = "#7f7f7f"
+tab.olive = "#bcbd22"
+tab.cyan = "#17becf"
 
 plot_cases<-function(observation.matrix, week.range, postcode.locations, col='#ffa500', cex=0.8, pch=NULL){
   weeks<-week.range[1]:week.range[2]
