1c57b9faf0f31feb40431927347649715ac952d3
Massimo Cavallaro <cavallaromass@gmail.com>
2021-10-26 17:14:33 +0100

    raw-dataset

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' A dataset containing the population and the centroid coordinates of all UK postcodes.
+#'
+#' @format A data frame with 1048575 rows and 7 variables:
+#' \describe{
+#'   \item{postcode}{}
+#'   \item{Total}{}
+#'   \item{latitude}{}
+#'   \item{longitude}{}
+#' }
+#' @source \url{https://www.ons.gov.uk}
+"postcode.data"
